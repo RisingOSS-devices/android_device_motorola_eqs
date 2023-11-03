3489ba99e36a590a6c0a01a3e8987ded5b505d3a
Nishant Kumar <www.rajsonu13@gmail.com>
2023-11-03 14:25:05 +0530

    Revert "eqs: Add external camera provider"
    This reverts commit 108754a1561daeb0a3b09d23b602e1f6972dec3f.

--- device.mk
+++ device.mk
@@ -55,10 +55,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/microphone_characteristics.xml:$(TARGET_COPY_OUT_VENDOR)/etc/microphone_characteristics.xml \
     $(LOCAL_PATH)/audio/usecaseKvManager.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usecaseKvManager.xml
 
-# Camera
-PRODUCT_COPY_FILES += \
-     $(LOCAL_PATH)/configs/external_camera_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/external_camera_config.xml
-
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service.eqs
