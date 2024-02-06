011103c1226a8a45eac76a920b762dcc4853444d
Nishant Kumar <www.rajsonu13@gmail.com>
2024-02-06 14:49:45 +0530

    eqs: Clone device specific display hal

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -9,6 +9,10 @@ if [ -n "${CLEAN_DT_REPOS}" ]; then
         unset CLEAN_DT_REPOS
     fi
 fi
+echo Cloning device specific HALs
+rm -rf hardware/qcom-caf/sm8450/display
+git clone https://github.com/Project-Elixir/hardware_qcom-caf_sm8450_display hardware/qcom-caf/sm8450/display
+echo Done.
 echo Pulling changes from git for device/motorola/eqs
 cd device/motorola/eqs && git reset --hard HEAD~1 && git pull;cd ../../..
 echo Done pulling changes, now proceeding to clone or pull changes of other repos
